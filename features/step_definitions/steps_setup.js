4145e5bfd4902deba0be419a7cbcb6a6939ac88d
abdulazizkarim11 <alikarim04@gmail.com>
2020-07-06 10:10:18 +0100

    default time out set and chromedriver installed

--- features/step_definitions/steps_setup.js
+++ features/step_definitions/steps_setup.js
@@ -1,47 +1,49 @@
 var {Given,Then,When} = require('cucumber');
 var homePage = require('../objects/object_setup');
+var {setDefaultTimeout} = require('cucumber');
+setDefaultTimeout(60 * 1000);
 
 
 
 Given(/^user is logged in$/, function () {
-    return homePage.userLogin()
+    return homePage.userLogin();
 
 });
 
 Then(/^user is on the products page$/,function () {
-    return homePage.productPage()
+    return homePage.productPage();
 
 });
 Then(/^user selects option price high to low from sort container$/, function () {
-    return homePage.productContainer()
+    return homePage.productContainer();
 
 });
 Given(/^user adds to cart sauce labs onesie$/, function () {
-    return homePage.onesieItem()
+    return homePage.onesieItem();
 
 });
 Given(/^user adds to cart sauce labs bike light$/, function () {
-    return homePage.bikeLight()
+    return homePage.bikeLight();
 
 });
 Given(/^user clicks on basket$/, function () {
-    return homePage.basket()
+    return homePage.basket();
 
 });
 Given(/^user clicks on checkout$/, function () {
-    return homePage.checkout()
+    return homePage.checkout();
 
 });
 Given(/^user enters checkout information$/, function () {
-    return homePage.checkoutInformation()
+    return homePage.checkoutInformation();
 
 });
 Given(/^user completes purchase journey$/, function () {
-    return homePage.purchaseComplete()
+    return homePage.purchaseComplete();
 
 
 });
 Given(/^user can see thank you message$/, function () {
-    return homePage.thankYouMessage()
+    return homePage.thankYouMessage();
 
 });
